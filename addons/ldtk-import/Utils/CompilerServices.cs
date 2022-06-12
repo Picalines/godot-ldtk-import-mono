6a11200577e0adfee0d42e0aad3130c054ac3d5f
Picalines <picalines0@gmail.com>
2022-06-13 02:08:08 +0300

    C# 10 & CompilerServices warning

--- addons/ldtk-import/Utils/CompilerServices.cs
+++ addons/ldtk-import/Utils/CompilerServices.cs
@@ -1,4 +1,12 @@
-﻿namespace System.Runtime.CompilerServices
+﻿#if !NET5_0_OR_GREATER
+using System.ComponentModel;
+
+namespace System.Runtime.CompilerServices
 {
+    [EditorBrowsable(EditorBrowsableState.Never)]
     public class IsExternalInit { }
 }
+
+#else
+#warning CompilerServices.IsExternalInit can be removed in .NET >=5
+#endif
